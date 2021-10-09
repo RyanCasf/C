adc216e2e3147b2eea591e71af0e5b7159089267
RyanCasf <ryaninternet62@gmail.com>
2021-10-09 09:17:43 -0300

    iostream output <<.

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
+// INCLUDE -> preprocessor to add files
 #include<stdio.h>
+#include<iostream>
 int main()
 {
-	printf("Ola mundo!");
+	printf("Ola mundo h!\n");
+	std::cout << "Ola mundo io!";
+	
 	return 0;
 }
