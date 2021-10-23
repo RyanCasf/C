b107508099dfba973d2c5bf54dfe0f6f8302fd80
RyanCasf <ryaninternet62@gmail.com>
2021-10-23 15:55:01 -0300

    out with f

---
+++ out.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+	int idade = 18;
+	float altura = 1.76;
+	char letraInicial = 'R';
+	
+	printf("DADOS\n\nIdade: %d\nAltura: %.2f\nInicial: %c",
+		idade, altura, letraInicial);
+	
+	return 0;
+}
